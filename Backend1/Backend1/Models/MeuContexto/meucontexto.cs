8aa5dba2cfe9e3b03f1087ef1054ab2805bc7987
mauro m suzuki <msndomauro@hotmail.com>
2017-10-18 20:50:04 -0200

    CONTEXTO

---
+++ Backend1/Backend1/Models/MeuContexto/meucontexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Backend1.Models.MeuContexto
+{
+	public class meucontexto : DbContext
+	{
+		public meucontexto(): base("strConn")
+		{
+			
+		}
+	}
+}
